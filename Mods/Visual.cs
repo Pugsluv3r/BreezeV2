dda8c6c8908346ca7e65326a51c1a50cd02a58a4
Pugs (laptop) <jordan@stopit.l>
2026-02-11 14:44:17 -0500

    Tracers (Maybe?)

---
+++ Mods/Visual.cs
@@ -0,0 +1,42 @@
+﻿using GorillaLocomotion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using static BreezeV2.Classes.SimpleInputs;
+
+
+namespace BreezeV2.Mods
+{
+    internal class Visual
+    {
+        public static void Tracersifrighttrigger() 
+        {
+            if (RightTrigger && NetworkSystem.Instance.InRoom)
+            {
+                foreach (var vrrig in GorillaParent.instance.vrrigs)
+                {
+                    if (vrrig.isLocal) continue;
+                    UnityEngine.LineRenderer line = vrrig.gameObject.GetComponent<UnityEngine.LineRenderer>();
+                    if (line == null) line = vrrig.gameObject.AddComponent<UnityEngine.LineRenderer>();
+                    line.SetPosition(0, GTPlayer.Instance.RightHand.controllerTransform.position);
+                    line.SetPosition(1, vrrig.transform.position);
+                    line.startWidth = 0.01f;
+                    line.endWidth = 0.01f;
+                    line.material.color = UnityEngine.Color.red;
+                    line.name = "BreezeTracers";
+                }
+                NetworkSystem.Instance.OnReturnedToSinglePlayer += () => 
+                {
+
+                    GameObject.Destroy(GameObject.Find("breezeTracers"));
+                };
+            }
+
+        }
+            
+    }
+}
